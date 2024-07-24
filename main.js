530cdccc251a7e58d9e139f073b7029f348950e3
HenTaku <134145323+HenTaku0@users.noreply.github.com>
2024-07-24 16:47:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var userAgent = $request.headers['User-Agent'] || $request.headers['user-agent']
 // 检查是否为图片类型并且请求头中不包含浏览器的标识
 if (contentType.includes('image') && !userAgent.includes('Mozilla')) {
     // 发送通知，包含图片的URL链接，可以在浏览器中打开
-    $notify("检测到指定域名图片", `URL: ${url}`, "点击查看图片", {"open-url": url});
+    $notify("🐱检测到Spotify歌词分享🐱", `URL: ${url}`, "点击查看图片", {"open-url": url});
 }
 
 // 返回未修改的响应体
