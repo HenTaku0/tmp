c64c0c6ff79fb5437bbc7b5198b567b0354261b7
HenTaku <134145323+HenTaku0@users.noreply.github.com>
2024-07-24 16:43:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,12 @@
 
 var url = $request.url;
 var contentType = $response.headers['Content-Type'] || $response.headers['content-type'];
+var userAgent = $request.headers['User-Agent'] || $request.headers['user-agent'];
 
-// 检查是否为图片类型
-if (contentType.includes('image')) {
-    // 获取持久化存储中的标记
-    var notified = $persistentStore.read("notified_storage_googleapis_com");
-
-    // 如果没有发送过通知，则发送通知
-    if (!notified) {
-        // 发送通知，包含图片的URL链接，可以在浏览器中打开
-        $notify("检测到指定域名图片", `URL: ${url}`, "点击查看图片", {"open-url": url});
-
-        // 设置持久化存储中的标记
-        $persistentStore.write("true", "notified_storage_googleapis_com");
-    }
+// 检查是否为图片类型并且请求头中不包含浏览器的标识
+if (contentType.includes('image') && !userAgent.includes('Mozilla')) {
+    // 发送通知，包含图片的URL链接，可以在浏览器中打开
+    $notify("检测到指定域名图片", `URL: ${url}`, "点击查看图片", {"open-url": url});
 }
 
 // 返回未修改的响应体
