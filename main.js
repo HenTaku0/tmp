60af5e512de35460db42818318cc0a536301b8ed
HenTaku <134145323+HenTaku0@users.noreply.github.com>
2024-07-24 16:37:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,10 @@
 
 var url = $request.url;
 var contentType = $response.headers['Content-Type'] || $response.headers['content-type'];
+var referer = $request.headers['Referer'] || $request.headers['referer'];
 
-// 检查是否为图片类型
-if (contentType.includes('image')) {
+// 检查是否为图片类型并且不是来自浏览器的直接访问
+if (contentType.includes('image') && !referer) {
     // 发送通知，包含图片的URL链接，可以在浏览器中打开
     $notify("检测到指定域名图片", `URL: ${url}`, "点击查看图片", {"open-url": url});
 }
