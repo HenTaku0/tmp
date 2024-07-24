46cd0b700f8069520a1280068c3f63c77249ade9
HenTaku <134145323+HenTaku0@users.noreply.github.com>
2024-07-25 05:57:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-// storage_googleapis_com_open_image.js
-
 var url = $request.url;
 var contentType = $response.headers['Content-Type'] || $response.headers['content-type'];
 var userAgent = $request.headers['User-Agent'] || $request.headers['user-agent'];
 
 // 检查是否为图片类型并且请求头中不包含浏览器的标识
 if (contentType.includes('image') && !userAgent.includes('Mozilla')) {
-    // 发送通知，包含图片的URL链接，可以在浏览器中打开
+    // 打开浏览器，显示图片URL
     $notify("🐱检测到Spotify歌词分享🐱", `URL: ${url}`, "点击查看图片", {"open-url": url});
+    // 直接跳转到浏览器
+    $done({response: {status: 302, headers: {Location: url}}});
+} else {
+    // 返回未修改的响应体
+    $done({});
 }
-
-// 返回未修改的响应体
-$done({});
