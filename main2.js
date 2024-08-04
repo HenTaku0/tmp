58d4f2e7d1829dde0e8cad5ebd5e80690840eae9
HenTaku <134145323+HenTaku0@users.noreply.github.com>
2024-08-04 18:41:17 +0800

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,15 +1,12 @@
-import { notify, done } from 'stash';
-
-// 获取请求和响应头信息
-const url = request.url;
-const contentType = response.headers['Content-Type'] || response.headers['content-type'];
-const userAgent = request.headers['User-Agent'] || request.headers['user-agent'];
+const url = $request.url;
+const contentType = $response.headers['Content-Type'] || $response.headers['content-type'];
+const userAgent = $request.headers['User-Agent'] || $request.headers['user-agent'];
 
 // 检查是否为图片类型并且请求头中不包含浏览器的标识
 if (contentType.includes('image') && !userAgent.includes('Mozilla')) {
     // 发送通知，包含图片的URL链接，可以在浏览器中打开
-    notify("🐱检测到Spotify歌词分享🐱", `URL: ${url}`, "点击查看图片", {"open-url": url});
+    $notification.post("🐱检测到Spotify歌词分享🐱", `URL: ${url}`, "点击查看图片", { "open-url": url });
 }
 
 // 返回未修改的响应体
-done({});
+$done({});
