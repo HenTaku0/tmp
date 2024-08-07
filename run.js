86bc3b149163cafc57e909f630f2e59b338619fb
HenTaku <134145323+HenTaku0@users.noreply.github.com>
2024-08-07 23:30:47 +0800

    Update run.js

--- run.js
+++ run.js
@@ -4,24 +4,12 @@ const userAgent = $request.headers['User-Agent'] || $request.headers['user-agent
 
 // 检查是否为图片类型并且请求头中不包含浏览器的标识
 if (url.includes("storage.googleapis.com") && contentType.includes('image') && !userAgent.includes('Mozilla')) {
-    const lastImageUrl = $persistentStore.read("last_image_url");
-
-    // 检查是否为新图片
-    if (lastImageUrl !== url) {
-        $persistentStore.write(url, "last_image_url");
-        console.log(`检测到新图片: ${url}`);
-
-        // 更新面板内容
-        $done({
-            title: "检测到图片",
-            content: `点击查看图片: ${url}`,
-            icon: url,
-            url: url,
-            backgroundColor: "#663399"
-        });
-    } else {
-        $done({});
-    }
+    console.log(`检测到图片且非浏览器请求: ${url}`);
+    // 发送通知，包含图片的URL链接，可以在浏览器中打开
+    $notification.post("检测到图片", `URL: ${url}`, "点击查看图片", { "open-url": url });
+    // 返回未修改的响应体
+    $done({});
 } else {
+    // 返回未修改的响应体
     $done({});
 }
