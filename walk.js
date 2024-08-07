d4e20e313405324a22c4df96b3498b4167cf7881
HenTaku <134145323+HenTaku0@users.noreply.github.com>
2024-08-07 23:14:22 +0800

    Update walk.js

--- walk.js
+++ walk.js
@@ -1,5 +1,5 @@
 name: "🌐 图片检测器"
-desc: "检测 storage.googleapis.com 的图片并发送通知"
+desc: "检测 storage.googleapis.com 的图片并显示在面板中"
 author: "Your Name"
 category: "Utility"
 
@@ -9,11 +9,19 @@ http:
 
   script:
     - match: ^https?://storage\.googleapis\.com/.*$
-      name: detect_and_redirect_image
+      name: detect_and_display_image
       type: response
       require-body: false
 
+tiles:
+  - name: detect_and_display_image
+    interval: 600
+    title: "等待检测到的图片"
+    content: "暂无图片"
+    icon: "photo"
+    backgroundColor: "#663399"
+
 script-providers:
-  detect_and_redirect_image:
-    url: https://raw.githubusercontent.com/HenTaku0/tmp/main/run.js
+  detect_and_display_image:
+    url: https://your-script-url/detect_and_display_image.js
     interval: 86400
