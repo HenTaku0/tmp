1d88c8fdac87a1f0f905c33714aea7f6ba7d11fc
HenTaku <134145323+HenTaku0@users.noreply.github.com>
2024-08-07 20:53:15 +0800

    Update run.js

--- run.js
+++ run.js
@@ -1,20 +1,20 @@
-name: "🌐 图片检测器"
-desc: "检测 storage.googleapis.com 的图片并发送通知"
-author: "Your Name"
-category: "Utility"
+const targetDomain = "storage.googleapis.com"; // 目标域名
+const url = $request.url;
+const contentType = $response.headers['Content-Type'] || $response.headers['content-type'];
 
-http:
-  mitm:
-    - "storage.googleapis.com"
+console.log("执行响应脚本");
+console.log("URL: " + url);
+console.log("Content-Type: " + contentType);
 
-  script:
-    - match: ^https?://storage\.googleapis\.com/.*$
-      name: detect_and_notify_image
-      type: response
-      require-body: false
-      argument:
+// 检查 URL 是否包含目标域名并且响应类型是图片
+if (url.includes(targetDomain) && contentType.includes('image')) {
+    console.log("检测到指定域名的图片");
 
-script-providers:
-  detect_and_notify_image:
-    url: https://raw.githubusercontent.com/HenTaku0/tmp/main/walk.js
-    interval: 86400
+    // 发送通知，包含图片的URL链接，可以在浏览器中打开
+    $notification.post("检测到图片", `URL: ${url}`, "点击查看图片", { "open-url": url });
+} else {
+    console.log("未检测到指定域名或响应不是图片");
+}
+
+// 返回未修改的响应体
+$done({});
