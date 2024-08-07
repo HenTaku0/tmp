8a9767d700cc797c379f8a0e40879973f7448be4
HenTaku <134145323+HenTaku0@users.noreply.github.com>
2024-08-07 23:13:42 +0800

    Update run.js

--- run.js
+++ run.js
@@ -4,11 +4,14 @@ const userAgent = $request.headers['User-Agent'] || $request.headers['user-agent
 
 // 检查是否为图片类型并且请求头中不包含浏览器的标识
 if (url.includes("storage.googleapis.com") && contentType.includes('image') && !userAgent.includes('Mozilla')) {
-    // 发送通知，包含图片的URL链接，可以在浏览器中打开
-    $notification.post("检测到图片", `URL: ${url}`, "点击查看图片", { "open-url": url });
-
-    // 直接跳转到浏览器
-    $done({ response: { status: 302, headers: { Location: url } } });
+    // 更新 Tile 面板内容
+    $persistentStore.write(url, "last_image_url");
+    $done({
+        title: "检测到图片",
+        content: "点击查看图片",
+        icon: url,
+        url: url
+    });
 } else {
     // 返回未修改的响应体
     $done({});
