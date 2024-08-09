510e78fc9b0c13aad9b94da1e9bc12e701f9620a
HenTaku <134145323+HenTaku0@users.noreply.github.com>
2024-08-09 13:44:46 +0800

    Update CheckSCDNRequestsTile.js

--- CheckSCDNRequestsTile.js
+++ CheckSCDNRequestsTile.js
@@ -3,13 +3,15 @@ var latestImageUrl = $persistentStore.read("latestSpotifyImage");
 console.log("读取到的最新图片 URL: " + latestImageUrl); // 输出读取到的 URL
 
 if (latestImageUrl) {
+    // 更新 Tile 内容并删除持久化存储中的 URL
     $done({
-        title: '最新聆听的 Spotify 专辑',
-        content: latestImageUrl,
+        title: '最近聆听的专辑',
+        content: '点击查看图片',
         icon: latestImageUrl,
         backgroundColor: '#1DB954',
         url: latestImageUrl
     });
+    $persistentStore.write("", "latestSpotifyImage"); // 删除持久化存储中的 URL
 } else {
     $done({
         title: 'Spotify 图片',
