9f8cf5e5d0cade24a6d8053183d2d661bf5f6f6c
HenTaku <134145323+HenTaku0@users.noreply.github.com>
2024-08-09 13:59:59 +0800

    Update CheckSCDNRequestsTile.js

--- CheckSCDNRequestsTile.js
+++ CheckSCDNRequestsTile.js
@@ -3,7 +3,7 @@ if (existingImages.length > 0) {
     let latestImageUrl = existingImages[existingImages.length - 1]; // 显示最后一张图片
     $done({
         title: '最新 Spotify 图片',
-        content: '点击查看图片',
+        content: '点击 查看图片',
         icon: latestImageUrl,
         backgroundColor: '#1DB954',
         url: latestImageUrl
